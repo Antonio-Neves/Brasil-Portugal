e25523d48d7cd6f2d979bdfd46f29fbd2d957057
antonio <it-an@hotmail.com>
2021-07-07 18:46:18 -0300

    Added Principal global context

--- principal/views.py
+++ principal/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from principal.models import Category
+from principal.models import Category, SubCategory, Article
+
+
+def global_principal_content(request):
+	"""
+	Return context for use in all templates.
+	"""
+	global_principal_context = {
+		'global_categories': Category.objects.all(),
+		'global_subcategories': SubCategory.objects.all(),
+		'global_articles': Article.objects.all()
+	}
+
+	return global_principal_context
 
 
 class IndexView(TemplateView):
 
 	def get(self, request, *args, **kwargs):
-		context = {'categories': Category.objects.all()}
-
-		print(context)
+		context = {
+			'categories': Category.objects.all(),
+			'subcategories': SubCategory.objects.all(),
+			'articles': Article.objects.all()
+		}
 
 		return render(request, 'principal/index.html', context)
 
 
 def content(request):
 
-	return render(request, 'principal/content.html', {})
+	return render(request, 'principal/content.html')
+
