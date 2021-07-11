ba63cff4416a7c6888e4071d2840027aba0252bf
antonio <it-an@hotmail.com>
2021-07-11 14:33:03 -0300

    Created Principal urls

--- principal/urls.py
+++ principal/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from principal.views import IndexView, content
-
+from principal.views import (
+	IndexView,
+	CategoryDetailView,
+	SubCategoryDetailView,
+	ArticleDetailView,
+)
 
 urlpatterns = [
 	path('', IndexView.as_view(), name='index'),
-	path('conteudo/', content, name='content')
+	path('category/<slug:slug>/',
+		 CategoryDetailView.as_view(), name='category-detail'),
+	path('subcategory/<slug:slug>/',
+		 SubCategoryDetailView.as_view(), name='subcategory-detail'),
+	path('article/<slug:slug>/',
+		 ArticleDetailView.as_view(), name='article-detail'),
 ]
